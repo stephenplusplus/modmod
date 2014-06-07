2f96ddbad5d01cfef2b4995c4e30429a52e4f39f
Stephen Sawchuk <sawchuk@gmail.com>
2014-06-07 09:41:47 -0400

    modmod works with symlinks now!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   var builtinLibs = require('repl')._builtinLibs;
   return [].slice.call(arguments).reduce(function (acc, key) {
-    var path = builtinLibs.indexOf(key) > -1 ? key : process.cwd() + '/node_modules/' + key;
+    var path = builtinLibs.indexOf(key) > -1 ? key : __dirname.replace('modmod', '') + key;
     acc[key] = require(path);
     return acc;
   }, {});
